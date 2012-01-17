310c76de8378a6813e8859dc0fca836a0b31c026
Paul Vorbach <paul@vorb.de>
2012-01-17 18:35:58 +0100

    Enable binary output redirection

--- bin/yamlprompt.js
+++ bin/yamlprompt.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 
 var yamlprompt = require('../yamlprompt.js');
+process.stdin.resume();
 var rl = require('readline').createInterface(process.stdin, process.stderr);
+rl.setPrompt('yaml> ', 6);
 
 yamlprompt(rl, function (err, obj) {
   rl.close();
@@ -9,7 +11,7 @@ yamlprompt(rl, function (err, obj) {
   if (err)
     return console.error(err);
   try {
-    console.log(JSON.stringify(obj));
+    console.log(JSON.stringify(obj, null, '\t'));
   } catch (err) {
     return console.error(err);
   }
