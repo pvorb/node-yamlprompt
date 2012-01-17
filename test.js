1380898b3e72da1714c47075744c2e6c5ccfcfc0
Paul Vorbach <paul@vorb.de>
2012-01-17 15:18:16 +0100

    Readline interface as first parameter

--- test.js
+++ test.js
@@ -1,6 +1,8 @@
 var yp = require('./yamlprompt.js');
+var rl = require('readline').createInterface(process.stdin, process.stdout);
+rl.setPrompt('> ', 2);
 
-yp('> ', function (err, obj) {
+yp(rl, function (err, obj) {
   if (err)
     throw err;
   console.log(obj);
--- yamlprompt.js
+++ yamlprompt.js
@@ -4,29 +4,31 @@
 var readline = require('readline');
 var yamlparser = require('yamlparser');
 
-function yamlprompt(prompt, cb) {
+function yamlprompt(rl, cb) {
   var yaml = '';
 
-  // create prompt
-  var rl = readline.createInterface(process.stdin, process.stdout);
-  rl.setPrompt(prompt, prompt.length);
-  rl.on('line', function (line) {
+  try {
+    // create prompt
+    rl.on('line', function (line) {
 
-    // try to parse yaml on '...'
-    if (line.trim() == '...') {
-      rl.close();
-      process.stdin.destroy();
-      try {
-        var obj = yamlparser.eval(yaml);
-        return cb(null, obj);
-      } catch (err) {
-        return cb(err);
+      // try to parse yaml on '...'
+      if (line.trim() == '...') {
+        rl.close();
+        process.stdin.destroy();
+        try {
+          var obj = yamlparser.eval(yaml);
+          return cb(null, obj);
+        } catch (err) {
+          return cb(err);
+        }
       }
-    }
 
-    yaml += line + '\n';
-    rl.prompt();
-  });
+      yaml += line + '\n';
+      rl.prompt();
+    });
+  } catch (err) {
+    return cb(err);
+  }
 
   rl.prompt();
 };
