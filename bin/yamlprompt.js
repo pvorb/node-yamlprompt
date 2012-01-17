b02a5fcf36bf48847afdc55a990c8878bc2ad984
Paul Vorbach <paul@vorb.de>
2012-01-17 18:54:29 +0100

    Add file parsing

--- bin/yamlprompt.js
+++ bin/yamlprompt.js
@@ -1,5 +1,21 @@
 #!/usr/bin/env node
 
+if (process.argv.length >= 3) {
+  var fs = require('fs');
+  var path = require('path');
+  var yamlparser = require('yamlparser');
+
+  try {
+    var f = fs.readFileSync(path.resolve(process.cwd(), process.argv[2]),
+        'utf8');
+    console.log(JSON.stringify(yamlparser.eval(f), null, '  '));
+    process.exit(0);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+}
+
 var yamlprompt = require('../yamlprompt.js');
 process.stdin.resume();
 var rl = require('readline').createInterface(process.stdin, process.stderr);
